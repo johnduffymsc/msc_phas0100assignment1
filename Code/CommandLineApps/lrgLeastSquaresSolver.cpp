85466dace6995a95c256861579b927b7b9986bac
John Duffy <john.duffy.19@ucl.ac.uk>
2020-03-09 21:43:14 +0000

    Backup of work in progress.

--- Code/CommandLineApps/lrgLeastSquaresSolver.cpp
+++ Code/CommandLineApps/lrgLeastSquaresSolver.cpp
@@ -29,7 +29,7 @@ int main(int argc, char** argv)
   // Parse the command line arguments.
   //
   
-  CLI::App app{"A program to perform Linear Regression."};
+  CLI::App app{"A command line interface program to perform Linear Regression. Data is supplied from a data file via the --file option, or randomly generated if the --file option is not given. The data file format is a space delimited 'x y' pair, one pair on each line. In the absence of the --file option, linear data is generated with user specified intercept and gradient values, together with the mean and and standard deviation of random noise which is applied to the linear data. The method used to solve the linear regression is one of 'normal_equations' or 'gradient_descent', which is selected via the --solver option. The program returns the intercept and gradient of the linear regression. Additionally, the program generates a Gnuplot script which can be used with Gnuplot to generate a PNG plot of the data and resultant regression line."};
 
   std::string filename = "";
   CLI::Option *filename_option = app.add_option("-f,--file", filename, "Data file, space separated X and y pairs, one pair per line.");
@@ -43,34 +43,58 @@ int main(int argc, char** argv)
   
   CLI11_PARSE(app, argc, argv);
 
+  // At this point we are guaranteed that:
+  //
+  // 1. filename_option is either (a) "", or (b) "filename_of_a_file_that_exists"
+  //
+  // 2. solver_option is either "normal_equations" or "gradient_descent"
+  //
+  // Let's continue on this basis.
 
+  
   //
   // Get the data.
   //
 
   lrg::vector_of_pairs data;
-  
-  try {
-    lrg::FileLoaderDataCreator creator (filename);
-    data = creator.GetData();
-  }
-  catch (lrg::Exception& e) {
-    std::cerr << "Caught lrg::Exception: " << e.GetDescription() << std::endl;
-  }
-  catch (std::exception& e) {
-    std::cerr << "Caught std::exception: " << e.what() << std::endl;
-  }
 
+  if (filename == "") {
+    // Generate random data.
+    ;
+  }
+  else {
+    // Read data from a file wrapping IO operations in a try block.
+    try {
+      lrg::FileLoaderDataCreator creator (filename);
+      data = creator.GetData();
+    }
+    catch (lrg::Exception& e) {
+      std::cerr << "Caught lrg::Exception: " << e.GetDescription() << std::endl;
+    // TODO: return EXIT_FAILURE
+    }
+    catch (std::exception& e) {
+      std::cerr << "Caught std::exception: " << e.what() << std::endl;
+    // TODO: return EXIT_FAILURE
+    }
+  }
+  
 
   //
   // Linear regression.
   //
 
   lrg::single_pair theta;
-  
-  lrg::NormalEquationSolverStrategy solver_strategy;
-  theta = solver_strategy.FitData(data);
 
+  if (solver == "normal_equations") {
+    // Normal Equations Solver.
+    lrg::NormalEquationSolverStrategy solver_strategy;
+    theta = solver_strategy.FitData(data);
+  }
+  else {
+    // Gradient Descent Solver.
+    ;
+  }
+  
 
   //
   // Print result.
@@ -97,9 +121,14 @@ int main(int argc, char** argv)
       f  << p.first << " " << p.second << std::endl;
     }
     f << "e" << std::endl;
-  } catch (...) {
-    // TODO(John): Improve below.
-    std::cout << "Caught Exception!." << std::endl;
+  }
+  catch (lrg::Exception& e) {
+    std::cerr << "Caught lrg::Exception: " << e.GetDescription() << std::endl;
+    // TODO: return EXIT_FAILURE
+  }
+  catch (std::exception& e) {
+    std::cerr << "Caught std::exception: " << e.what() << std::endl;
+    // TODO: return EXIT_FAILURE
   }
 
 
